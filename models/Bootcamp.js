def703e0eca9bc908a135ba11137ccc935ad2f5e
Stephen Bevis <stevbev95@gmail.com>
2022-05-09 22:24:50 -0600

    Added Course Routes

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -100,6 +100,13 @@ const BootcampSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
      }
+}, {
+   toJSON: {
+      virtuals: true
+   },
+   toObject: {
+      virtuals: true
+   }
 })
 
 // Create Bootcamp Slug From Name
@@ -133,5 +140,18 @@ BootcampSchema.pre('save', async function(next){
    next()
 })
 
+// Delete Associated Courses When Bootcamp is Deleted
+BootcampSchema.pre('remove', async function(next){
+   await this.model('Course').deleteMany({ bootcamp: this._id })
+   next()
+})
+
+// Reverse Populate With Virtuals
+BootcampSchema.virtual('courses', {
+   ref: 'Course',
+   localField: '_id',
+   foreignField: 'bootcamp',
+   justOne: false
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
